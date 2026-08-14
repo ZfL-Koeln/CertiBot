ea9190b240a6e1fcb60a88327a04e6d3d7e9f422
Jan Wieners <mail@jan-wieners.de>
2026-08-14 12:15:02 +0200

    Fix path.

--- encrypt/encrypt-participants.ts
+++ encrypt/encrypt-participants.ts
@@ -7,5 +7,5 @@ const fileName = 'example.txt';
 const names = fs.readFileSync(`./participants/${fileName}`, 'utf8').split('\n').filter(Boolean);
 const encryptedNames = names.map((name: any) => CryptoJS.AES.encrypt(name, password).toString());
 
-fs.writeFileSync(`../public/participants/${fileName}`, encryptedNames.join('\n'), 'utf8');
+fs.writeFileSync(`./public/participants/${fileName}`, encryptedNames.join('\n'), 'utf8');
 console.log('Namen wurden verschlüsselt gespeichert.');
